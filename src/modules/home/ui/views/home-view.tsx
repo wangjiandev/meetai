d485f0dad3db6e39f6d3f3459934fe6e0c3e84cc
Wang Jian <wangjian0504@gmail.com>
2025-06-07 23:56:44 +0800

    trpc setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,35 +1,14 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
-import { Card, CardContent } from '@/components/ui/card'
-import { useRouter } from 'next/navigation'
+import { useTRPC } from '@/trpc/client'
+import { useQuery } from '@tanstack/react-query'
 
 const HomeView = () => {
-  const router = useRouter()
-  const { data: session } = authClient.useSession()
-
-  const handleSignOut = async () => {
-    authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push('/sign-in')
-        },
-      },
-    })
-  }
-
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.hello.queryOptions({ text: 'wangjian' }))
   return (
-    <div className="flex min-h-svh w-full flex-col items-center justify-center gap-4">
-      {!!session && (
-        <Card className="w-96">
-          <CardContent className="flex flex-col gap-4">
-            <p>Name: {session.user.name}</p>
-            <p>Email: {session.user.email}</p>
-            <Button onClick={handleSignOut}>Sign Out</Button>
-          </CardContent>
-        </Card>
-      )}
+    <div className="flex flex-col gap-y-4 p-4">
+      <p>{data?.greeting}</p>
     </div>
   )
 }
