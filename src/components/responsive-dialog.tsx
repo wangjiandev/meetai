ee3fdd52d558c4e1517f5c272f2cec1e6b0f604d
Wang Jian <wangjian0504@gmail.com>
2025-06-08 15:08:50 +0800

    responsive diglog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useIsMobile } from '@/hooks/use-mobile'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from '@/components/ui/drawer'
+
+interface ResponsiveDialogProps {
+  children: React.ReactNode
+  title: string
+  description: string
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+const ResponsiveDialog = ({ children, title, description, open, onOpenChange }: ResponsiveDialogProps) => {
+  const isMobile = useIsMobile()
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default ResponsiveDialog
